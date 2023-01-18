0aa7704dbabd048a6f13de3064ec4b3ebc9fae99
Harsh Kumar <harshkum1899@gmail.com>
2023-01-18 12:38:19 +0530

    day 10 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/themes.dart';
 import 'package:flutter_application_1/utilities/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -19,13 +20,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // ignore: prefer_const_constructors
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        //fontFamily: GoogleFonts.lato().fontFamily,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyThemes.lightTheme(context),
+      darkTheme: MyThemes.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -8,7 +8,7 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     // ignore: prefer_const_declarations
     final imageURL =
-        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F439%2F863%2Foriginal%2Fvector-users-icon.jpg&imgrefurl=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fuser-avatar&tbnid=1CA8jr9jkE7GgM&vet=12ahUKEwi41arsps78AhXDSXwKHe4nBGAQMygAegUIARDiAQ..i&docid=GX3OEghUv3ZhTM&w=5120&h=5120&q=user%20avatar&ved=2ahUKEwi41arsps78AhXDSXwKHe4nBGAQMygAegUIARDiAQ";
+        "https://static.vecteezy.com/system/resources/thumbnails/005/544/770/small/profile-icon-design-free-vector.jpg";
     return Drawer(
       child: Container(
         color: Colors.amber,
@@ -25,8 +25,8 @@ class MyDrawer extends StatelessWidget {
                 decoration:
                     // ignore: prefer_const_constructors
                     BoxDecoration(color: Colors.black),
-                // ignore: prefer_const_constructors
-                accountName: Text("Harsh Kumar"),
+
+                accountName: const Text("Harsh Kumar"),
                 // ignore: prefer_const_constructors
                 accountEmail: Text("harshkum1899@gmail.com"),
                 currentAccountPicture: CircleAvatar(
